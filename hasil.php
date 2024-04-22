b57ac242b5ddfed2e0099b8cc02a5ce454573c4e
faqih <114202179+Faqih555@users.noreply.github.com>
2024-04-22 21:02:13 +0800

    Add files via upload

--- hasil.php
+++ hasil.php
@@ -1,38 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8">      
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Hasil Perhitungan Kalkulator Online</title><!--untuk menambahkan judul pada web-->
+    <link href="style.css" rel="stylesheet"type= "text/css"><!--untuk menghubungkan dengan css-->
+    
+    
 </head>
 <body>
-    <div>
-        <h1>Hasil</h1>
 
-        <?php
-            if(isset($_GET['angka1']) && isset($_GET['angka2']) && isset($_GET['operator'])) {
-                $angka1 = $_GET['angka1'];
-                $angka2 = $_GET['angka2'];
-                $operator = $_GET['operator'];
-                $hasil = '';
-                
-                if($operator == '+' ){
-                   $hasil =  $angka1 + $angka2;
-                } else if($operator == '-' ){
-                    $hasil =  $angka1 - $angka2;
-                 } else if($operator == ':' ){ 
-                    $hasil =  $angka1 / $angka2;
-                 } else if($operator == 'x' ){ 
-                    $hasil =  $angka1 * $angka2;
-                 }
-                
-                echo $angka1 . " " . $operator . " " . $angka2 . " = " . $hasil;
-            } else {
-                echo "angka tidak dimasukkan"; 
-            }
-        ?>
-        <br>
-        <a href="index.php">Kembali</a> 
-    </div>
+<form >
+    <h1>Hasil Perhitungan</h1><!--untuk mengidentifikasi judul dan sub judul dari sebuah alamat web-->
+    <?php
+    $angka1 = $_GET['angka1'];/*get dalam bahasa program php digunakan untuk mengirim data yang sifatnya sensitif*/
+    $angka2 = $_GET['angka2'];/*get dalam bahasa program php digunakan untuk mengirim data yang sifatnya sensitif*/
+    $operator = $_GET['operator'];/*get dalam bahasa program php digunakan untuk mengirim data yang sifatnya sensitif*/
+
+    if ($operator == '+') { /*fungsi if berfungsi untuk menjalankan suatu segmen fungsi berdasarkan pemeriksaan kondisi tertentu melalui operator logical */
+        $hasil = $angka1 + $angka2; 
+    }else if($operator == '-') { /*else if digunakan untuk pengontrolan terhadap kondisi yang lain dalam satu blok if */
+        $hasil = $angka1 - $angka2;
+    
+}
+echo $angka1 . $operator. $angka2 . '='. $hasil; /*echo berfungsi untuk menampilkan hasil teks ke layar*/
+    ?>
+    <a href="index.php">Kembali</a><!--untuk menentukan url halaman yang di tuju oleh link tersebut-->
+    <a type="button" onclick="return window.print();">Cetak</a><!--untuk menentukan url halaman yang di tuju oleh link tersebut-->
+
+
+
+</form>
+
+   
 </body>
 </html>
--- index.php
+++ index.php
@@ -3,21 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Kalkulator online</title>
-</head>
-<body class="background">
+    <title>Kalkulator Online</title> <!-- title untuk menambahkan judul pada web-->
+    <link href="style.css" rel="stylesheet"type= "text/css"><!--untuk menghubungkan css-->
 
-<form action="hasil.php">
-    <input type="number" name="angka1" class="ukuran kotak">
-    <select name="operator" id="" >
-        <option value="">+</option>
-        <option value="">-</option>
-        <option value="">:</option>
-        <option value="">x</option>
-    </select>
-    <input type="number" name="angka2" class="ukuran">
-    <button type="submit">hasil</button>
+</head>
+<body>
+
+<form action="hasil.php"><!--untuk menentukann lokasi url atau file script
+ yang akan memperoses data yang di kirim dari form ketika di submit-->
+    <h1>Kalkulator Online</h1><!--untuk mengidentifikasi judul dan sub judul dari sebuah alamat web-->
+    
+    <input type="number" name="angka1"><!--untuk memasukkan angka dan input type number akan
+    memvalidasi dan menolak masukan selain angka-->
+    <select name="operator"><!--di gunakan untuk mengambil data dari tabel tersebut-->
+        <option>+</option><!--untuk menampung data yang isinya bisa berpotensi kosong(none)-->
+        <option>-</option><!--untuk menampung data yang isinya bisa berpotensi kosong(none)-->
+        
+</select>
+<input type="number" name="angka2"><!--untuk memasukkan angka dan input type number akan
+    memvalidasi dan menolak masukan selain angka-->
+<button type="submit">Hitung</button><!--untuk mengirimkann formulir-->
 </form>
 
 </body>
