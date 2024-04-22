049b47125fb16a6e5f14d4ec417862c76aafedec
faqih <114202179+Faqih555@users.noreply.github.com>
2024-04-22 20:59:36 +0800

    Add files via upload

--- hasil.php
+++ hasil.php
@@ -3,30 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hasil Perhitungan</title>
+    <title>Document</title>
 </head>
 <body>
-
     <div>
-        <H2>Hasil Perhitungan Kalkulator Online</H2>
+        <h1>Hasil</h1>
+
         <?php
-            $angka1 = $_GET['angka1'];
-            $angka2 = $_GET['angka2'];
-            $operator = $_GET['operator'];
-
-            if($operator == '+') {
-                $hasil =  $angka1 + $angka2;
-            }else if($operator == '-') {
-                $hasil =  $angka1 - $angka2;
-            }else if($operator == ':') {
-                $hasil =  $angka1 / $angka2;
-            }else if($operator == 'x') {
-                $hasil =  $angka1 * $angka2;
+            if(isset($_GET['angka1']) && isset($_GET['angka2']) && isset($_GET['operator'])) {
+                $angka1 = $_GET['angka1'];
+                $angka2 = $_GET['angka2'];
+                $operator = $_GET['operator'];
+                $hasil = '';
+                
+                if($operator == '+' ){
+                   $hasil =  $angka1 + $angka2;
+                } else if($operator == '-' ){
+                    $hasil =  $angka1 - $angka2;
+                 } else if($operator == ':' ){ 
+                    $hasil =  $angka1 / $angka2;
+                 } else if($operator == 'x' ){ 
+                    $hasil =  $angka1 * $angka2;
+                 }
+                
+                echo $angka1 . " " . $operator . " " . $angka2 . " = " . $hasil;
+            } else {
+                echo "angka tidak dimasukkan"; 
             }
-            echo $angka1 . $operator . $angka2 . '=' . $hasil;
         ?>
-        <a href="index.php">Kembali</a>
+        <br>
+        <a href="index.php">Kembali</a> 
     </div>
-
 </body>
 </html>
--- index.php
+++ index.php
@@ -3,21 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kalkulator onlen</title>
-    <link href="style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="style.css">
+    <title>Kalkulator online</title>
 </head>
-<body>
-    <form action="hasil.php">
-        <h2> Kalkulator </h2>
-        <input type="number" name="angka1">
-        <select name="operator">
-            <option>+</option>
-            <option>-</option>
-            <option>:</option>
-            <option>x</option>
-        </select>
-        <input type="number" name="angka2">
-        <button type="submit">Hitung</button>
-    </form>
+<body class="background">
+
+<form action="hasil.php">
+    <input type="number" name="angka1" class="ukuran kotak">
+    <select name="operator" id="" >
+        <option value="">+</option>
+        <option value="">-</option>
+        <option value="">:</option>
+        <option value="">x</option>
+    </select>
+    <input type="number" name="angka2" class="ukuran">
+    <button type="submit">hasil</button>
+</form>
+
 </body>
 </html>
